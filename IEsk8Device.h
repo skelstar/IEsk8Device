ff8a2d31156e2d25285f20d00a59d5dff27f460d
Sean <skelstar@gmail.com>
2019-11-28 17:58:54 +1300

    Add files via upload

--- IEsk8Device.h
+++ IEsk8Device.h
@@ -5,20 +5,21 @@ class IEsk8Device
 
 public:
   typedef void (*callBack)();
- 
+  typedef void (*notifyCallBack)(const uint8_t *data, uint8_t data_len);
+
   virtual void initialise();
   virtual void connect() = 0;
 
   virtual void setOnConnectedEvent(callBack ptr_onConnectedEvent);
   virtual void setOnDisconnectedEvent(callBack ptr_onDisconnectedEvent);
-  virtual void setOnNotifyEvent(callBack ptr_onNotifyEvent);
+  virtual void setOnNotifyEvent(notifyCallBack ptr_onNotifyEvent);
   virtual void setOnSentEvent(callBack ptr_onSentEvent);
 
 // protected:
   
   callBack _onConnectedEvent;
   callBack _onDisconnectedEvent;
-  callBack _onNotifyEvent;
+  notifyCallBack _onNotifyEvent;
   callBack _onSentEvent;
 };
 
